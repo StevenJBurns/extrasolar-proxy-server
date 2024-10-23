06b1744dccc227bb514da2a19a178729f17a8f39
SJB <stevenjburns@msn.com>
2024-10-22 22:07:29 -0500

    refactor: structure of the Express server + router

--- src/createServer.js
+++ src/createServer.js
@@ -1,37 +1,16 @@
 import cors from 'cors';
 import express from 'express';
 import { createRouter } from './routes/router.js';
-// import { fetchNasaData } from './fetchNasaData.js';
-import { fetchStars } from './handlers/fetchStars.js';
-import { fetchPlanets } from './handlers/fetchPlanets.js';
 
 export const createServer = () => {
-  const server = express();
-  const router = createRouter();
+  const server = createRouter(express());
 
-  router.get('/stars', fetchStars);
-
-  router.get('/planets', fetchPlanets);
-
-  router.get('/health', (_req, res) => {
-    res.status(200).send('Health ChecK : Good\n');
-  });
-
-  // router.get('/get', async (req, res, next) => {
-  //   const data = await fetchNasaData();
-  //   res.json(data);
-  //   next();
-  // });
-
-  router.use('*', (_req, res) => res.status(404).send('Wut?  404 - Not Found'));
-
-  server.set('port', process.env.SERVER_PORT ?? 3000).disable('x-powered-by');
+  server.disable('x-powered-by');
+  server.set('port', process.env.SERVER_PORT ?? 3000);
   server.use([express.json(), express.urlencoded({ extended: false })]);
 
   server.use(cors({
     origin: [
-      // 'http://localhost:5500',
-      // 'http://127.0.0.1:5500',
       'http://localhost:4096',
       'http://127.0.0.1:4096',
       'https://stevenjburns.github.io'
@@ -42,8 +21,6 @@ export const createServer = () => {
       'GET',
     ],
   }));
-
-  server.use(router);
 
   return server;
 };
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,15 +1,26 @@
 import { Router } from "express";
-import * as Handlers from "../handlers/index.js";
+import * as Controllers from "../controllers/index.js";
 
-export const createRouter = () => {
+export const createRouter = (server) => {
   const router = new Router();
-  const { fetchNasaData, fetchNasaDataById, fetchStars, fetchPlanets, healthHandler } = Handlers;
 
-  router.get('/solar-systems', fetchNasaData);
-  router.get('/solar-systems:id', fetchNasaDataById);
+  const {
+    basicLogger,
+    fetchSolarSystems,
+    fetchSolarSystemsById,
+    fetchStars,
+    fetchPlanets,
+    healthHandler
+  } = Controllers;
+
+  router.use(basicLogger);
+  router.get('/solar-systems', fetchSolarSystems);
+  router.get('/solar-systems/:hostname', fetchSolarSystemsById);
   router.get('/stars', fetchStars);
   router.get('/planets', fetchPlanets);
   router.get('/health', healthHandler );
 
-  return router;
+  router.use('*', (_req, res) => res.status(404).send('Wut?  404 - Not Found\n'));
+
+  return server.use(router);
 };
