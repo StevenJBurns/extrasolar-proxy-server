2c4c7dcda8ac4add2a9ed25473140c2cea072e1c
SJB <stevenjburns@msn.com>
2024-10-23 21:37:23 -0500

    refactor: utility folder re-structure

--- src/utilitity/curry.js
+++
@@ -1,9 +0,0 @@
-export function curry(fn) {
-  const arity = fn.length;
-
-  return function $curry(...args) {
-    if (args.length < arity) return $curry.bind(null, ...args);
-
-    return fn.call(null, ...args);
-  };
-};
