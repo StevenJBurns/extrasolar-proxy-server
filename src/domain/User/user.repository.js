4d61b3506d4441c48aa6808e78a6b3c1eb03eefc
SJB <stevenjburns@msn.com>
2024-11-07 21:10:46 -0600

    feat: early boilerplate for user Repository

---
+++ src/domain/User/user.repository.js
@@ -0,0 +1,24 @@
+export const repository = () => {
+  const store = new Set();
+
+  const add = () => {};
+
+  const get = () => {};
+
+  const getAll = () => {};
+
+  const update = () => {};
+
+  const remove = () => {};
+
+  const removeAll = () => {};
+
+  return {
+    add,
+    get,
+    getAll,
+    update,
+    remove,
+    removeAll,
+  }
+};
